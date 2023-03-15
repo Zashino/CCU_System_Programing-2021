c48ef52818d57bb0fe7bbe3a763406e9636ace5c
Zashino <41558343+Zashino@users.noreply.github.com>
2023-03-15 15:35:11 +0800

    Add files via upload

---
+++ LAB3/buffersize16384.c
@@ -0,0 +1,25 @@
+#include "apue.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define	BUFFSIZE	16384
+
+int main(void)
+{
+		int	n, fd;
+		char	buf[BUFFSIZE];
+
+		fd = open("/dev/null",O_RDWR|O_CREAT);
+
+		while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+				if (write(fd, buf, n) != n)
+						err_sys("write error");
+
+		if (n < 0)
+				err_sys("read error");
+
+		exit(0);
+}
+
---
+++ LAB3/fsyncbuffersize4096.c
@@ -0,0 +1,27 @@
+#include "apue.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define	BUFFSIZE	4096
+
+int main(void)
+{
+		int	n, fd;
+		char	buf[BUFFSIZE];
+
+		fd = open("/dev/null",O_RDWR|O_CREAT);
+
+		while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) {
+				fsync(fd);
+				if (write(fd, buf, n) != n)
+						err_sys("write error");
+		}
+
+		if (n < 0)
+				err_sys("read error");
+
+		exit(0);
+}
+
